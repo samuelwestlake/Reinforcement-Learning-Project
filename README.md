18105c4e31d89726d37ac7765c11d8a868af23c8
samuelwestlake <samuel.westlake@postgrad.plymouth.ac.uk>
2017-10-28 15:34:19 +0100

    Update README and email address

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Reinforcement-Learning-Project
 Implementation of a Nearest Sequence Memory (NSM) algorithm applied to a partially observable version of McCallum's grid-world.
+
+Episodes are simulated for both an agent taking random actions, and an agent implementing NSM.  
+Summaries for each agent are plotted after completion. 
--- src/main.m
+++ src/main.m
@@ -1,12 +1,14 @@
 % Samuel Westlake
-% samuel.westlake@postgrad.plymouth.ac.uk
+% samuel.westlake@hotmail.co.uk
 % Reinforcement Learning Assignment
 % Implementation of a Reinforcement Learning algorithm for solving
 % partially observable Markov decision problems. 
+
 global goal_state;
 global mem_size;
 global chance;
 global k;
+
 goal_state = 2;                     % Number of the goal state 
 mem_size = 20;                      % Max nb of rows in an 
 chance = 10;                        % % chance of taking random action
@@ -22,7 +24,7 @@ plot(rndTrial(nb_episodes));        % Plot steps for 100 random
 title('Steps taken to solve through random actions')
 xlabel(xlab)
 ylabel(ylab)
-axis([0, nb_episodes, 0, 500])      % 8Set axis limits
+axis([0, nb_episodes, 0, 500])      % Set axis limits
 subplot(1, 2, 2)                    % Second plot in 1 x 2
 plot(NSMTrial(nb_episodes));        % Plot steps for 100 NSM 
 title('Steps taken to solve through NSM actions')
